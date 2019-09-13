54d92aea2dd6264fd73b407682174e70b114a171
kazi.serikzhan@gmail.com <Dalgrad2020>
2019-09-13 14:31:23 -0300

    11602 TLE

--- 11602.cpp
+++ 11602.cpp
@@ -12,19 +12,30 @@ using namespace std;
 char which[1<<20],buff[N],layout[A+1];
 const char *a= "_abcdefghilmnoprstu";
 int id[256],w[A],target,c[A],len,pos[A],
-    highest_freq[1<<A];
+    highest_freq[1<<A],sum_freq[1<<A],
+    smallest_possible[1<<A],
+    largest_possible[1<<A];
 unsigned int present,same_frequency[N];
 
 bool f( unsigned int covered, int i, int cur_cost ) {
     if ( i == len )
         return cur_cost == target;
+    assert( i < len );
     if ( cur_cost > target )
         return false ;
     assert( i < len and covered != present );
-    unsigned int remaining= (present&~covered);
+    auto remaining= ~covered & present;
     if ( (target-cur_cost) < (i+1)*w[highest_freq[remaining]] )
         return false ;
-    for ( unsigned int ch, prev= MASK(A+1), u= (present&~covered); u; u&= ~same_frequency[prev= w[id[ch]]] ) {
+    if ( (i+1)*sum_freq[~covered & present]+cur_cost > target )
+        return false ;
+    if ( A*sum_freq[~covered & present]+cur_cost < target )
+        return false ;
+    if ( sum_freq[remaining]*i+smallest_possible[remaining]+cur_cost > target )
+        return false ;
+    if ( sum_freq[remaining]*(A-__builtin_popcount(remaining))+largest_possible[remaining]+cur_cost < target )
+        return false ;
+    for ( unsigned int ch, prev= MASK(A+1), u= remaining; u; u&= ~same_frequency[prev= w[id[ch]]] ) {
         ch= a[which[L(u)]];
         assert( w[id[ch]] != prev );
         pos[id[layout[i] = ch]] = i;
@@ -32,7 +43,7 @@ bool f( unsigned int covered, int i, int cur_cost ) {
         if ( f(covered|L(u), i+1, cur_cost+(pos[x]+1)*w[x]) )
             return true;
     }
-    return false ;
+    return f(covered,i+1,cur_cost);
 }
 
 int main() {
@@ -48,9 +59,26 @@ int main() {
         for ( len= 0, present= 0, i= 0; buff[i] and buff[i] != '\n'; ++i )
             present|= BIT(id[buff[i]]);
         for ( u= present; u; u= (u-1)&present )
-            for ( highest_freq[u]= -1, v= u; v; v&= ~L(v) )
-                if ( highest_freq[u] == -1 or w[highest_freq[u]] < w[which[L(v)]] )
-                    highest_freq[u]= which[L(v)];
+            for (highest_freq[u] = -1, v = u; v; v &= ~L(v))
+                if (highest_freq[u] == -1 or w[highest_freq[u]] < w[which[L(v)]])
+                    highest_freq[u] = which[L(v)];
+        for ( u= 1; u < (1<<A); ++u )
+            sum_freq[u]= sum_freq[u&~L(u)]+w[which[L(u)]];
+        int indices[A];
+        for ( u= 0; u < (1 << A); ++u ) {
+            for ( k= 0, i= 0; i < A; ++i )
+                if ( u & BIT(i) )
+                    indices[k++]= i;
+            sort(indices,indices+k,[&]( const auto &a, const auto &b )->bool {
+                return w[a] < w[b];
+            });
+            smallest_possible[u]= largest_possible[u]= 0;
+            for ( i= 0; i < k; ++i )
+                largest_possible[u] += (i+1)*w[indices[i]];
+            for ( j= 0, i= k-1; i >= 0; --i, ++j )
+                smallest_possible[u]+= (j+1)*w[indices[i]];
+            assert( smallest_possible[u] <= largest_possible[u] );
+        }
         memset(same_frequency,0,sizeof same_frequency);
         for ( u= present; u; u&= ~L(u) )
             same_frequency[w[which[L(u)]]]|= L(u);
