972eb1c22130e13917b02fc8b46be0d5ab846804
kazi.serikzhan@gmail.com <Dalgrad2020>
2019-09-14 16:35:13 -0300

    11602 Accepted!

--- 11602.cpp
+++ 11602.cpp
@@ -1,5 +1,8 @@
 //
 // Created by sj on 13/09/19.
+// 11602: SMS for Blinds
+// TOPIC: bitmasks, rearrangement inequality, bitwise operations, DP, pruning, backtracking
+// status: Accepted
 //
 #include <bits/stdc++.h>
 #define BIT(k) (1ULL<<(k))
@@ -12,14 +15,11 @@ using namespace std;
 
 char which[1<<20],buff[N],layout[A+1],alphabet[A+1],bts[1<<20],kth[1<<A][A];
 const char *a= "_abcdefghilmnoprstu";
-int w[256],target,c[A],len,pos[A],
-    highest_freq[1<<A],sum_freq[1<<A],
+int w[256],target,new_id[256],
+    sum_freq[1<<A],
     smallest_possible[1<<A],
     largest_possible[1<<A];
 unsigned int present,same_frequency[N];
-int perm[A],inverse_perm[A],new_id[256];
-unordered_set<int> possible_sum[1<<A],cache[1<<A][A];
-bool seen[1<<A][A];
 
 bool f( unsigned int covered, int i, int cur_cost ) {
     auto remaining= ((~covered) & present);
@@ -37,7 +37,7 @@ bool f( unsigned int covered, int i, int cur_cost ) {
     int low= i, high= A-bts[remaining], mid;
     assert( sum_freq[remaining]*high+largest_possible[remaining]+cur_cost >= target );
     int j= i;
-#if 0
+#if 1
     if ( sum_freq[remaining]*low+largest_possible[remaining]+cur_cost >= target )
         j= low;
     else {
@@ -51,22 +51,8 @@ bool f( unsigned int covered, int i, int cur_cost ) {
     if ( sum_freq[remaining]*j+smallest_possible[remaining]+cur_cost > target )
         return false ;
     int sum_needed= target-cur_cost-sum_freq[remaining]*j;
-#if 0
-    if ( 2 <= A-j and A-j <= 4 and bts[remaining] == (A-j) and possible_sum[remaining].find(sum_needed) == possible_sum[remaining].end() )
-        return false ;
-#endif
-#if 0
-    if ( 2 <= bts[remaining] and bts[remaining] <= C and seen[remaining][A-j] and cache[remaining][A-j].find(sum_needed) != cache[remaining][A-j].end() )
-        return false ;
-#endif
     for ( int l= i; l <= j-1; layout[l++]= '\0' ) ;
     auto discarded= remaining & ~remaining;
-#if 0
-    if ( 2 <= bts[remaining] and bts[remaining] <= C ) {
-        seen[remaining][A - j] = true;
-        cache[remaining][A - j].insert(sum_needed);
-    }
-#endif
     low= 0, high= bts[remaining]-1;
     unsigned int initial= remaining, rr= remaining&~BIT(kth[remaining][low]);
     if ( w[alphabet[kth[remaining][low]]]*(j+1)+sum_freq[rr]+smallest_possible[rr]+cur_cost <= target )
@@ -102,7 +88,7 @@ bool f( unsigned int covered, int i, int cur_cost ) {
 }
 
 int main() {
-    int i,j,k,l;
+    int i,j,k,l,n;
     unsigned int u,v;
     int absent[A];
 #ifndef ONLINE_JUDGE
@@ -115,66 +101,43 @@ int main() {
         for ( kth[u][0]= which[L(u)], k= 1; k < bts[u]; ++k )
             kth[u][k]= 1+kth[u>>1][k-(u&1)];
     for ( layout[A]= '\0' ;fgets(buff,sizeof buff,stdin) and 0[buff] != '*'; ) {
-        for ( memset(w,0,sizeof w), i= 0; buff[i] and buff[i] != '\n'; ++w[buff[i++]] ) ;
+        for ( n= 0, memset(w,0,sizeof w), i= 0; buff[i] and buff[i] != '\n'; ++w[buff[i++]], ++n ) ;
         for ( l= 0; a[l]; alphabet[l]= a[l], ++l ) ;
         sort(alphabet,alphabet+A,[&]( const char &x, const char &y)->bool {
             return w[x] < w[y];
         });
-#if 0
-        for ( u= 0; u < (1<<A); ++u ) {
-            if (  bts[u] > C or bts[u] < 2 ) continue ;
-            possible_sum[u].clear();
-            for ( i= 0; i < A; ++i )
-                seen[u][i]= false, cache[u][i].clear();
-        }
-#endif
         for ( i= 0; i < A-1; ++i )
             assert(w[alphabet[i]] <= w[alphabet[i+1]]);
         for ( i= 0; i < A; ++i )
             new_id[alphabet[i]]= i;
-        for ( len= 0, present= 0, i= 0; buff[i] and buff[i] != '\n'; ++i )
+        for ( present= 0, i= 0; buff[i] and buff[i] != '\n'; ++i )
             present|= BIT(new_id[buff[i]]);
         for ( u= 1; u < (1<<A); ++u )
             sum_freq[u]= sum_freq[u&~L(u)]+w[alphabet[which[L(u)]]];
-        for ( u= present; u; u= (u-1)&present ) {
-            int indices[A];
-            for ( k= 0, i= 0; i < A; ++i )
-                if ( u & BIT(i) )
-                    indices[k++]= i;
-            smallest_possible[u]= largest_possible[u]= 0;
-            for ( i= 0; i < k; ++i )
-                largest_possible[u] += (i+1)*w[alphabet[indices[i]]];
-            for ( j= 0, i= k-1; i >= 0; --i, ++j )
-                smallest_possible[u]+= (j+1)*w[alphabet[indices[i]]];
+        for ( u= 0; u < (1<<A); ++u ) {
+            if ( u != (u&present) )
+                continue ;
+            if ( !(u&(u-1)) ) {
+                largest_possible[u]= smallest_possible[u]= sum_freq[u];
+                continue ;
+            }
+            v= u & ~L(u);
+            largest_possible[u]= largest_possible[v]+sum_freq[v]+w[alphabet[which[L(u)]]];
+            smallest_possible[u]= smallest_possible[v]+w[alphabet[which[L(u)]]]*(1+bts[v]);
             assert( smallest_possible[u] <= largest_possible[u] );
         }
-        memset(same_frequency,0,sizeof same_frequency);
+        for ( i= 0; i <= n; same_frequency[i++]= 0 ) ;
         for ( u= present; u; u&= ~L(u) )
             same_frequency[w[alphabet[which[L(u)]]]]|= L(u);
         for ( i= 0, l= 0; i < A; ++i )
             if ( !(present & BIT(i)) )
                 absent[l++]= i;
         sscanf(fgets(buff,sizeof buff,stdin),"%d",&target);
-#if 0
-        for ( u= 0; u < (1<<A); ++u )
-            if ( bts[u] >= 2 and bts[u] <= 4 ) {
-                int indices[4];
-                for ( k= 0, i= 0; i < A; ++i )
-                    if ( u & BIT(i) )
-                        indices[k++]= i;
-                do {
-                   int sm= 0;
-                   for ( i= 0; i < k; ++i )
-                       sm+= (i+1)*w[alphabet[indices[i]]];
-                   possible_sum[u].insert(sm);
-                } while ( next_permutation(indices,indices+k) ) ;
-            }
-#endif
         assert( f(0,0,0) );
         for ( i= 0, l= 0; l < A; ++l )
             if ( !layout[l] )
                 layout[l]= alphabet[absent[i++]];
-        layout[A]= '\0', printf("%s\n",layout);
+        layout[A]= '\0', puts(layout);
     }
     return 0;
 }
